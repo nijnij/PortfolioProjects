565b06de06e65a007ca1f65262ec95a6e04ba7a8
nijnij <145539561+nijnij@users.noreply.github.com>
2023-11-08 18:23:52 +0100

    Update COVID Portfolio Project (Tableau).sql

--- COVID Portfolio Project (Tableau).sql
+++ COVID Portfolio Project (Tableau).sql
@@ -11,22 +11,8 @@ where continent is not null
 --Group By date
 order by 1,2
 
--- Just a double check based off the data provided
--- numbers are extremely close so we will keep them - The Second includes "International"  Location
-
-
---Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
---From PortfolioProject..CovidDeaths
-----Where location like '%states%'
---where location = 'World'
-----Group By date
---order by 1,2
-
 
 -- 2. 
-
--- We take these out as they are not inluded in the above queries and want to stay consistent
--- European Union is part of Europe
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths$
@@ -47,7 +33,6 @@ order by PercentPopulationInfected desc
 
 
 -- 4.
-
 
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths$
